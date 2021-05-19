e6f904e49c23267c234ceb35568fe9c3d647af4c
MrRojito-Dev <cancinojuaco@gmail.com>
2021-05-19 01:10:12 -0400

    Comando eval añadido

--- src/commands/developer/eval.js
+++ src/commands/developer/eval.js
@@ -1,3 +1,4 @@
+const Util = require('util');
 module.exports = {
     name: 'eval',
     aliases: ['e', 'ev'],
@@ -5,5 +6,16 @@ module.exports = {
     args: 1,
     uso: 'eval <Código JS>',
     run: (Discord, client, message, args) => {
+        try {
+            let output = eval(args.join(' '))
+            let type = typeof output;
+            if (typeof output !== 'string') output = Util.inspect(output, { depth: 0 })
 
+            if (output.length >= 1020) output = `${output.substr(0, 1010)}...`;
+
+            return message.channel.send(`Tipo: ${type.substring(0, 1).toUpperCase() + type.substring(1)}\n${output.replace(client.token, 'ajindasjidna')}`, { code: "js" })
+        } catch (err) {
+            let embed3 = new Discord.MessageEmbed()
+            return message.channel.send(`Tipo: ${err.name}\n${err.message}`, { code: "js" })
+        }
 }}
