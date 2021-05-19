eed76857f954cd0d5a97ae93113c30e8391296de
MrRojito-Dev <cancinojuaco@gmail.com>
2021-05-18 23:56:52 -0400

    Cambios en la configuración de express y views

--- src/index.js
+++ src/index.js
@@ -8,10 +8,18 @@ const routes = require(`./web/routes/routes.js`);
 // Settings
 app.set('port', 3000);
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, `./web/views`));
+app.set('views', path.join(__dirname, `web/views`));
+app.engine('html', require('ejs').renderFile);
+app.use(express.urlencoded({ extended: true }));
+
+// Static
+app.use(express.static(path.join(__dirname, 'web/public')));
 
 // Routes
 app.use('/', routes);
+app.get('*', (req, res) => {
+    res.status(404).sendFile(`${__dirname}/views/404.html`);
+});
 
 app.listen(app.get('port'), () => console.log(`Express server listen on ${app.get('port')} port`));
 
